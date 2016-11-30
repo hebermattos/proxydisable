2f835776fe45059f64da4884f0bf49ecdc6769ac
heber.mattos <heber.mattos@HEBER>
2016-11-30 17:39:09 -0200

    Add project files.

---
+++ ProxyDisable/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Threading;
+
+namespace ProxyDisable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+
+            var value = (int)registry.GetValue("ProxyEnable");
+
+            if (value == 1)
+                registry.SetValue("ProxyEnable", 0);
+
+
+        }
+    }
+}
---
+++ ProxyDisableService/Service1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace ProxyDisableService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Timer _timer;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _timer = new Timer(3000); 
+            _timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+            _timer.Start();
+        }
+
+        private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {     
+            var registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+
+            var value = (int)registry.GetValue("ProxyEnable");
+
+            if (value == 1)
+            {
+                registry.SetValue("ProxyEnable", 0);
+                EventLog.WriteEntry("ProxyEnable: 0");
+            }
+        }
+
+        protected override void OnStop()
+        {
+            _timer.Dispose();
+            
+        }
+    }
+}
